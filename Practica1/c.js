385c6d9dca1cf95c0603ffe2ad5a336286a3a3e9
Mario Rodarte <mariohrodarte@outlook.com>
2025-05-19 15:59:27 -0600

    Ejercicio C

---
+++ Practica1/c.js
@@ -0,0 +1,10 @@
+/*
+Crea una arrow function llamada saludoPersonalizado que reciba dos
+parámetros: nombre y edad, y retorne una cadena como la siguiente
+*/
+
+// "Hola, me llamo Isay y tengo 37 años."
+
+const saludoPersonalizado = (nombre, edad) => "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+
+console.log(saludoPersonalizado("Mario",20));
