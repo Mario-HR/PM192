b5156a51b1288b290238a76d4240e5ee2c12fd7c
Mario Rodarte <mariohrodarte@outlook.com>
2025-05-19 15:54:17 -0600

    Ejercicio B

---
+++ Practica1/b.js
@@ -0,0 +1,6 @@
+const cuadrado = numero => numero*numero;
+
+// muestra en consola probando con tres numeros diferentes
+console.log(cuadrado(2));
+console.log(cuadrado(5));
+console.log(cuadrado(8));
