fa74952d39e208109a4aa2ded254a4c10da2b223
Mario Rodarte <mariohrodarte@outlook.com>
2025-05-19 15:54:02 -0600

    Ejercicio A

---
+++ Practica1/a.js
@@ -0,0 +1,9 @@
+let nombre = "Armando";
+let edad = 25;
+
+nombre = "Ana María";
+
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+// muestra en consola el contenido de saludo
+console.log(saludo);
