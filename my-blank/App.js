d61e31b35a6390587e1fcfc700fac063f742e881
Mario Rodarte <mariohrodarte@outlook.com>
2025-06-11 18:31:04 -0600

    Práctica 5 - Parte 2

--- my-blank/App.js
+++ my-blank/App.js
@@ -3,9 +3,9 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 const Texto=(props)=>{
-  const {contenido}=props
+  const {children}=props
   return(
-    <Text>{contenido}</Text>
+    <Text>{children}</Text>
   )
 }
 
@@ -13,9 +13,9 @@ const Texto=(props)=>{
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto contenido="Hola"></Texto>
-      <Texto contenido="Mundo"></Texto>
-      <Texto contenido='React Native'></Texto>
+      <Texto>Hola</Texto>
+      <Texto>Mundo</Texto>
+      <Texto>React Native</Texto>
       <Button title='Presioname'></Button>
       <StatusBar style="auto" />
     </View>
