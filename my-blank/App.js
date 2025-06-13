a1b993b5d3fa27543b8e382757d932f2403ec9c4
Mario Rodarte <mariohrodarte@outlook.com>
2025-06-12 18:43:40 -0600

    Práctica 6

--- my-blank/App.js
+++ my-blank/App.js
@@ -3,11 +3,12 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import React,{useState} from 'react';
 
-const Texto=()=>{
+const Texto=(props)=>{
+  const {style}=props
   const [contenido,setContenido]=useState("Hola Mundo React")
   const actualizarTexto=()=>{setContenido('Estado actualizado')}
   return(
-    <Text onPress={actualizarTexto}>{contenido}</Text>
+    <Text style={[styles.text, style]} onPress={actualizarTexto}>{contenido}</Text>
   )
 }
 
@@ -15,9 +16,9 @@ const Texto=()=>{
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto></Texto>
-      <Texto></Texto>
-      <Texto></Texto>
+      <Texto style={styles.red}></Texto>
+      <Texto style={styles.green}></Texto>
+      <Texto style={styles.blue}></Texto>
       <Button title='Presioname'></Button>
       <StatusBar style="auto" />
     </View>
@@ -29,7 +30,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'base-line',
     justifyContent: 'center',
+    flexDirection: 'column'
   },
+  text: {
+    color: 'green',
+    fontSize: 20,
+  },
+  red: {backgroundColor: 'red'},
+  green: {backgroundColor: 'green'},
+  blue: {backgroundColor: 'blue'},
 });
