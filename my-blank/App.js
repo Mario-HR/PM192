e3148c9b6b27fd482b795425e5ae55e585d4bdaf
Mario Rodarte <mariohrodarte@outlook.com>
2025-06-17 23:48:58 -0600

    Práctica 7 - BottomSheet

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,6 +1,7 @@
 /* Zona 1: importaciones */
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
 import React,{useState} from 'react';
 
 const Texto=(props)=>{
@@ -14,12 +15,22 @@ const Texto=(props)=>{
 
 /* Zona 2: Main */
 export default function App() {
+  const bottomSheetRef = useRef(null)
+
+  // Puntos de interrupción (snap points)
+  const snapPoints = useMemo(() => ['25%', '50%'], [])
+
   return (
     <View style={styles.container}>
       <Texto style={styles.red}></Texto>
       <Texto style={styles.green}></Texto>
       <Texto style={styles.blue}></Texto>
-      <Button title='Presioname'></Button>
+      <Button title="Abrir Bottom Sheet" onPress={() => bottomSheetRef.current?.expand()}/>
+      <BottomSheet ref={bottomSheetRef} index={-1} snapPoints={snapPoints}>
+        <View style={styles.bottomSheet}>
+          <Text>Hola desde el Bottom Sheet!</Text>
+        </View>
+      </BottomSheet>
       <StatusBar style="auto" />
     </View>
   );
@@ -41,4 +52,8 @@ const styles = StyleSheet.create({
   red: {backgroundColor: 'red'},
   green: {backgroundColor: 'green'},
   blue: {backgroundColor: 'blue'},
+  bottomSheet: {
+    flex: 1,
+    alignItems: 'center',
+  },
 });
