522eac3668c7a2375252a959d4c95b70f078517d
Mario Rodarte <mariohrodarte@outlook.com>
2025-06-18 19:03:25 -0600

    Práctica 8 - Button

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,82 +1,59 @@
 /* Zona 1: importaciones */
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Switch } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { use, useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, Image } from 'react-native';
 
 /* Zona 2: Main */
 export default function App() {
-  const [activarSwitch, setActivarSwitch] = useState(false);
-  const [modoOscuro, setModoOscuro] = useState(false);
+  const [botonDesactivado,setBotonDesactivado]=useState(false);
+  const [contador,setContador]=useState(0);
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={[styles.contenedor, modoOscuro && styles.fondoOscuro]}>
-        {/* Aqui van los componentes */}
-        <Text style={[styles.titulo, modoOscuro && styles.textoClaro]}>Práctica con Switchs</Text>
-        <View style={styles.opcion}>
-          <Text style={[styles.etiqueta, modoOscuro && styles.textoClaro]}>Activar Switch 2</Text>
-          <Switch
-          value={activarSwitch}
-          onValueChange={setActivarSwitch}
-          trackColor={{false: '#ccc', true: '#4caf50'}}
-          thumbColor={activarSwitch?'#ffffff':'#999999'}
-          ></Switch>
-        </View>
-        <View style={styles.opcion}>
-          <Text style={[styles.etiqueta, modoOscuro && styles.textoClaro]}>Modo oscuro</Text>
-          <Switch
-          value={modoOscuro}
-          onValueChange={setModoOscuro}
-          trackColor={
-            !activarSwitch?{false:'#ff9999', true: '#ff3b30'}: {false: '#ccc', true: '#4caf50'}
-          }
-          thumbColor={
-            !activarSwitch
-            ?'#ff3b30':
-            modoOscuro
-            ?'#ffffff'
-            :'#999999'
-          }></Switch>
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View>
+      <Button title="Presioname" color='#841584' onPress={()=>alert('Me has presionado')}></Button>
+      <Button
+      title={botonDesactivado ? 'Desactivado':'Desactivame'}
+      disabled={botonDesactivado}
+      onPress={()=>setBotonDesactivado(true)}
+      >
+      </Button>
+      <View style={styles.contenedor}>
+        <Button title="Izquierda" color='#674323'></Button>
+        <Button title="Derecha" color='#097865'></Button>
+      </View>
+      <TouchableOpacity style={styles.dynamicButton} onPress={()=>setContador(contador+1)}>
+        <Text style={styles.dynamicText}>{contador}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={()=>alert('La pokebola ha sido presionada')}>
+        <Image source={require('./assets/pokebola.png')} style={styles.imagen}></Image>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 /* Zona 3: Estilos */
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'base-line',
-    justifyContent: 'center',
-    flexDirection: 'column'
-  },
   contenedor: {
-    flex: 1,
-    background: '#ffffff',
-    paddingHorizontal: 30,
-    justifyContent: 'center',
-  },
-  titulo: {
-    fontSize: 24,
-    marginBottom: 40,
-    textAlign: 'center',
-    fontWeight: 'bold'
-  },
-  fondoOscuro: {
-    backgroundColor: '#1a1a1a',
-  },
-  textoClaro: {
-    color: '#ffffff',
-  },
-  opcion: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 30,
-    alignItems: 'center',
+    justifyContent: 'space-between'
   },
-  etiqueta: {
+  boton: {
+    marginTop: 10,
+    alignItems: 'center',
+    backgroundColor: '#234354'
+  },
+  textoBoton: {
+    color: '#124354',
     fontSize: 18,
+  },
+  imagen: {
+    width: 100,
+    height: 100,
+  },
+  dynamicButton: {
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#987867',
+    borderRadius: 5,
+    alignItems: 'center',
   }
 });
